6471a903c745c492122886d8e248d39ecac4e9d3
Siddhant <sidsil45@gmail.com>
2025-08-11 12:18:47 +0545

    Bugs fixed

--- webinstaller.sh
+++ webinstaller.sh
@@ -3,9 +3,11 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y git
 cd
 sudo git clone https://github.com/SiddhantSilwal/RaspberryPI-GPIO-Web-Controller.git
-cd /RaspberryPi-GPIO-Web-Controller
+cd RaspberryPi-GPIO-Web-Controller
 chmod +x install.sh
 source ./install.sh
 instl
+cd
+cd RaspberryPi-GPIO-Web-Controller
 sudo rm -rf webinstaller.sh
 cd
